4eca7e3bee76eb90dc7ae5f105612be84af99bab
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-26 13:57:28 +0530

    add operate function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let op = "";
+
 function add(a, b) {
   return a + b;
 }
@@ -11,7 +15,22 @@ function divide(a, b) {
   return a / b;
 }
 
-console.log(add(10, 100));
-console.log(substract(20, 10));
-console.log(multiply(2, 10));
-console.log(divide(50, 5));
+function operate(operator, aNum, bNum) {
+  if (operator === "+") {
+    return add(aNum, bNum);
+  }
+  if (operator === "-") {
+    return substract(aNum, bNum);
+  }
+  if (operator === "*") {
+    return multiply(aNum, bNum);
+  }
+  if (operator === "/") {
+    return divide(aNum, bNum);
+  }
+}
+console.log(operate("/", 100, 200));
+// console.log(add(10, 100));
+// console.log(substract(20, 10));
+// console.log(multiply(2, 10));
+// console.log(divide(50, 5));
