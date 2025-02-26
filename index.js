c87fbf798f3825544aa761e0eabef4519a064422
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-26 13:45:47 +0530

    add basic arithmetic functions

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+function substract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(10, 100));
+console.log(substract(20, 10));
+console.log(multiply(2, 10));
+console.log(divide(50, 5));
